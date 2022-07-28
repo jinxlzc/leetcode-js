9851c25cfa1a3c570861d4bca5bcdf4c9dd61d4f
lizechen <zechen.li@insentek.com>
2022-07-28 15:00:17 +0800

    A 打卡

---
+++ leet2351.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]]) {
+            return s[i];
+        }
+        map[s[i]] = 1;
+    }
+    return null;
+};
+const s = 'abccbaacz';
+console.log(repeatedCharacter(s));
