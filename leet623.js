c7563cbb4c081bd8dc3ac445659a85f19391d3ab
lizechen <jinxlzc@foxmail.com>
2022-08-05 00:08:40 +0800

    A 打卡

---
+++ leet623.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function(root, val, depth) {
+    if (!root) {
+        return null;
+    }
+    if (depth === 1) {
+        return new TreeNode(val, root, null);
+    }
+    if (depth === 2) {
+        root.left = new TreeNode(val, root.left, null);
+        root.right = new TreeNode(val, null, root.right);
+    } else {
+        root.left = addOneRow(root.left, val, depth - 1);
+        root.right = addOneRow(root.right, val, depth - 1);
+    }
+    return root;
+};
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
